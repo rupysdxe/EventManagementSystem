893fa4494bc5efc37858b039f245063f23d5f877
Rupesh Dangi <83266954+rupysdxe@users.noreply.github.com>
2021-12-03 15:46:44 +0545

    Edit and delete done

--- src/main/java/com/example/eventmanagementsystem/controller/IndexController.java
+++ src/main/java/com/example/eventmanagementsystem/controller/IndexController.java
@@ -5,26 +5,34 @@ import com.example.eventmanagementsystem.repo.EventRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 import java.util.List;
 
 @Controller
 public class IndexController {
+
     private final EventRepo eventRepo;
+
     public IndexController(EventRepo eventRepo) {
         this.eventRepo = eventRepo;
     }
-    @GetMapping(value = "/")
+
+    @GetMapping(value = "/index")
+
     public String index(Model model){
+
         List<Event> eventList=eventRepo.findAll();
+
         StaticClass.addTimeValue(eventList);
+
         model.addAttribute("eventList",eventList);
+
         return "index";
+
     }
 
 
-    @GetMapping(value = "/login")
+
+    @GetMapping(value = "/")
     public String PagesLogin(){
         return "login";
     }
